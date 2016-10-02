a52aee7d679377417b084ae5bd7b1b9024756ea1
Luisa <lufeza95@gmail.com>
2016-10-01 22:02:05 -0700

    webpack working

--- js/clientApp.js
+++ js/clientApp.js
@@ -1,26 +1,18 @@
-/* global React ReactDOM */
-
+var React = require('react')
+var MyTitle = require('./MyTitle')
 var div = React.DOM.div
 var h1 = React.DOM.h1
 
-var MyTitle = React.createClass({
-  render () {
-    return (
-      div(null,
-        h1({style: {color: this.props.color}}, this.props.title)
-      )
-    )
-  }
-})
 
 var myTitlefactory = React.createFactory(MyTitle)
-var ce = React.createElement;
+var ce = React.createElement
 
 var MyFirstComponent = (
   div(null,
-    myTitlefactory({title : 'Props are grate', color: 'DARKTURQUOISE'}),
+    myTitlefactory({title: 'Props are grate', color: 'DARKTURQUOISE'}),
     React.createElement(MyTitle, {title: 'use props everywhere', color: 'mediumaquamarine'}),
     ce(MyTitle, {title: 'Props are the best', color: 'rebeccapurple'})
+
   )
 )
 
