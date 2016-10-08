6f022e4f1a13d1e44283597aa7049312469b3ea6
Luisa <lufeza95@gmail.com>
2016-10-07 18:36:08 -0700

    two routes now

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,11 +1,12 @@
 const React = require('react')
+const { Link } = require('react-router')
 
 const Landing = () => (
   <div className='app-container'>
     <div className='home-info'>
       <h1 className='title'>video</h1>
       <input className='search' type='text' placeholder='search' />
-      <button className='browse-all'> or browse all </button>
+      <Link to='/search' className='browse-all'> or browse all </Link>
     </div>
   </div>
 )
--- js/clientApp.jsx
+++ js/clientApp.jsx
@@ -2,12 +2,14 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const Landing = require('./Landing')
 const ReactRouter = require('react-router')
+const Search = require('./Search')
 
 const { Router, Route, hashHitory } = ReactRouter
 
 const App = () => (
   <Router history={hashHitory}>
     <Route path='/' component={Landing} />
+    <Route path='/search' component={Search} />
   </Router>
 )
 
