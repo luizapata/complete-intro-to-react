611254e26336d4999c9a140ea2ef1998ec898f04
Luisa <lufeza95@gmail.com>
2016-10-07 18:55:57 -0700

    displaying img

--- js/Search.jsx
+++ js/Search.jsx
@@ -3,9 +3,18 @@ const data = require('../public/data')
 
 const Search = () => (
   <div className='container'>
-    {data.shows.map((show) => (
-      <h3>{show.title}</h3>
-    ))}
+    <div className='shows'>
+      {data.shows.map((show) => (
+        <div className='show'>
+          <img src={`public/img/posters/${show.poster}`} className='show-img' />
+          <div className='show-text'>
+            <h3 className='show-title'>{show.title}</h3>
+            <h4 className='show-year'>{show.year}</h4>
+            <p className='show-description'>{show.description}</p>
+          </div>
+        </div>
+      ))}
+    </div>
   </div>
 )
 
